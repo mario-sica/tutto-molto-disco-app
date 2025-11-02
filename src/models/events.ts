d7de0938d7a2120ff648b26a7027565e19736fcf
mario-sica <mario97sica@gmail.com>
2025-11-02 16:31:02 +0100

    EventCards update

--- src/models/events.ts
+++ src/models/events.ts
@@ -1,6 +1,5 @@
 import type {EventCard, EventRow} from "./interfaces.ts";
 import image1 from '../assets/images/image00001.jpeg';
-import image2 from '../assets/images/image00002.jpeg';
 import image3 from '../assets/images/image00003.jpeg';
 import image4 from '../assets/images/image00004.jpeg';
 import image5 from '../assets/images/image00007.jpeg';
@@ -12,23 +11,16 @@ export const eventRows: EventRow[] = [
     {
         id: 1,
         image: image1,
-        title: 'Va bene dai',
+        title: 'La nostra storia',
         description: 'Bacon ipsum dolor amet meatloaf beef short loin ham brisket boudin jerky spare ribs shank kielbasa tail. Alcatra kevin meatball ribeye burgdoggen landjaeger short ribs bresaola brisket prosciutto kielbasa capicola.',
         imagePosition: 'left'
     },
     {
         id: 2,
-        image: image2,
-        title: 'E come mai',
+        image: image3,
+        title: 'La nostra missione',
         description: 'Bacon ipsum dolor amet meatloaf beef short loin ham brisket boudin jerky spare ribs shank kielbasa tail. Alcatra kevin meatball ribeye burgdoggen landjaeger short ribs bresaola brisket prosciutto kielbasa capicola.',
         imagePosition: 'right'
-    },
-    {
-        id: 3,
-        image: image3,
-        title: 'Ti piace assai',
-        description: 'Bacon ipsum dolor amet meatloaf beef short loin ham brisket boudin jerky spare ribs shank kielbasa tail. Alcatra kevin meatball ribeye burgdoggen landjaeger short ribs bresaola brisket prosciutto kielbasa capicola.',
-        imagePosition: 'left'
     }
 ];
 
